bf471949caefad80b4eb3614e2143be2b991ac6c
T-Pakorn <Pakorn.Thanaprachanon@outlook.com>
2021-01-28 18:05:32 +0700

    Add Week 03.

---
+++ Code/Week03_PalindromicPrime.java
@@ -0,0 +1,57 @@
+import java.util.ArrayList;
+/*
+* Testcase for ApexTone
+* Correctness of Output
+* */
+public class Week03_PalindromicPrime {
+    public static void main(String[] args) {
+        ArrayList<Integer> palindromicPrime = new ArrayList<>();
+
+        for(int i=2;;i++){
+            if(palindromicPrime.size() >= 100){
+                break;
+            }
+            if(isPrime(i)){
+                if(isPalindrome(String.valueOf(i))){
+                    palindromicPrime.add(i);
+                }
+            }
+        }
+        for(int i=1;i<=palindromicPrime.size();i++){
+            System.out.print(palindromicPrime.get(i-1)+" ");
+            if(i%10 == 0){
+                System.out.println();
+            }
+        }
+    }
+
+    public static boolean isPrime(int number){
+        number = Math.abs(number);
+        if(number == 2){
+            return true;
+        }
+        else if(number%2 == 0 || number == 1){
+            return false;
+        }
+        else{
+            for(int i=3;i<=Math.sqrt(number);i+=2){
+                if(number%i == 0){
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+
+    public static boolean isPalindrome(String s){
+        int left=0, right=s.length()-1;
+        while(left < right){
+            if(s.charAt(left) != s.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
